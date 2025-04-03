5152f8449158c416bb25577666fe77ac90764e73
Adil-M2004 <D00265739@student.dkit.ie>
2025-04-03 22:30:28 +0100

    Initial commit

---
+++ cmake-build-debug/Crawler.cpp
@@ -0,0 +1,11 @@
+//
+// Created by manzoor on 03/04/2025.
+//
+
+#include "Crawler.h"
+
+using namespace std;
+
+int id;
+Position position;
+
---
+++ cmake-build-debug/Crawler.h
@@ -0,0 +1,20 @@
+//
+// Created by manzoor on 03/04/2025.
+//
+
+#ifndef CRAWLER_H
+#define CRAWLER_H
+
+
+
+class Crawler {
+
+  public:
+    Crawler();
+
+
+};
+
+
+
+#endif //CRAWLER_H
