a73ab12baee5cd3629a29ab89bddd1b91cecf8e2
Adil-M2004 <D00265739@student.dkit.ie>
2025-04-04 14:48:42 +0100

    Adding postion struct and direction

--- cmake-build-debug/Crawler.h
+++ cmake-build-debug/Crawler.h
@@ -5,6 +5,23 @@
 #ifndef CRAWLER_H
 #define CRAWLER_H
 
+#include <iostream>
+#include <list>
+#include <vector>
+
+// Define the Position struct
+struct Position {
+    int x;
+    int y;
+};
+
+// Define the direction enum
+enum class Direction {
+    North = 1,
+    East,
+    South,
+    West
+};
 
 
 class Crawler {
